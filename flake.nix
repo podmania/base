1328982927844dd8797f7aa39041c418380ff06c
bryce <bryce.hoehn@icloud.com>
2026-05-09 16:20:07 -0400

    feat: add debug image variant with shared base-debug layer

--- flake.nix
+++ flake.nix
@@ -27,6 +27,47 @@
           WorkingDir = "/app";
         };
       };
+
+      base-debug-image = nix2container.packages.${system}.nix2container.buildImage {
+        name = "base";
+        tag = "latest-debug";
+        fromImage = self.packages.${system}.base-image;
+        copyToRoot = pkgs.buildEnv {
+          name = "debug-utils";
+          paths = with pkgs; [
+            bashInteractive
+            coreutils
+            findutils
+            gnugrep
+            gnused
+            gawk
+            curl
+            wget
+            iproute2
+            iputils
+            netcat
+            bind.dnsutils
+            socat
+            procps
+            strace
+            lsof
+            less
+            file
+            tree
+            gnutar
+            gzip
+            bzip2
+            xz
+            zstd
+            jq
+            which
+            nano
+            vim
+          ];
+          pathsToLink = [ "/bin" "/sbin" ];
+        };
+        maxLayers = 1;
+      };
     };
 
     defaultPackage.${system} = self.packages.${system}.base-image;
